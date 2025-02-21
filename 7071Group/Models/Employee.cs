f8035bf871c58812471d87950a7f45e8d76395be
derekd03 <134988441+derekd03@users.noreply.github.com>
2025-02-20 20:22:17 -0800

    Define Entity Models

---
+++ 7071Group/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace _7071Group.Models
+{
+    public class Employee
+    {
+        public int EmployeeID { get; set; } // PK 
+        public string? Name { get; set; }
+        public string? Address { get; set; }
+        public string? EmergencyContact { get; set; }
+        public string? JobTitle { get; set; }
+        public string? EmploymentType { get; set; } 
+        public decimal SalaryRate { get; set; } 
+        public int ReportsTo { get; set; } // FK
+
+    }
+}
